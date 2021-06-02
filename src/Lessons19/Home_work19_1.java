6c9873aac0bda65dc1f3203fa75336c96c3b9499
vitaliy <vitaliy.arm@gmail.com>
2021-06-02 15:16:59 +0300

    HomeWork - part of Lesson 19

--- src/Lessons19/Home_work19_1.java
+++ src/Lessons19/Home_work19_1.java
@@ -17,8 +17,9 @@ public class Home_work19_1 {
         //System.out.println(Arrays.toString(array));
 
         for (int i = 0; i < array.length; i++){
+            int min = array[i];
             for (int c = 0; c < array.length; c++) {
-                int min = array[i];
+
                 if (array[c] > min) {
                     min = array[c];
                     int change = array[i];
